e548d9932d35830714f656bea8f81a4b18f0e0ba
Abhishek Kanrar <abhishekkanrar007@gmail.com>
2018-06-17 21:29:50 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from "@angular/core";
+import { Subscription } from 'rxjs/Subscription';
+@Component({
+    selector: "my-app", template: "<h1>Hi Abhi  {{welcomeMessage}} is Working</h1>"
+})
+export class AppComponent {
+    welcomeMessage: string = " Angular 2"
+}
